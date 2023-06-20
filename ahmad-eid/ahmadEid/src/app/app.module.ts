5307a81e077dfcd66a8c5e9da89fcf64755579f7
ahmadeidali863 <ahmadeidali863@gmail.com>
2023-06-20 15:06:31 +0300

    create project ahmad portfolio

---
+++ ahmad-eid/ahmadEid/src/app/app.module.ts
@@ -0,0 +1,40 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { initializeApp } from 'firebase/app';
+import { HttpClientModule } from '@angular/common/http';
+
+//import { AngularFirestoreModule } from '@angular/fire/compat/firestore/'; 
+//import { AngularFireStorageModule } from '@angular/fire/compat/storage'; 
+import { AngularFireModule } from '@angular/fire/compat';
+import { getFirestore } from 'firebase/firestore';
+const firebaseConfig = {
+  apiKey: "AIzaSyBr8xhrGhQkqKGGiY9TR_yCJ8J7R887zPA",
+  authDomain: "ahmad-eid-portfolio.firebaseapp.com",
+  projectId: "ahmad-eid-portfolio",
+  storageBucket: "ahmad-eid-portfolio.appspot.com",
+  messagingSenderId: "704300199529",
+  appId: "1:704300199529:web:33d1f799ad58afcd5abcf8"
+};
+
+//export const app = initializeApp(firebaseConfig);
+//export const database = getFirestore(app);
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+   // AngularFirestoreModule,
+   // AngularFireStorageModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
