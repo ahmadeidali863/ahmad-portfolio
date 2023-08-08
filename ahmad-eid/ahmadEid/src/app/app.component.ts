19ed50220a0f153be0f086a0688d3743286c0025
ahmadeidali863 <ahmadeidali863@gmail.com>
2023-08-08 18:56:59 +0300

    userinfo

--- ahmad-eid/ahmadEid/src/app/app.component.ts
+++ ahmad-eid/ahmadEid/src/app/app.component.ts
@@ -1,22 +1,121 @@
-import { Component, HostListener } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { AuthService } from './core/services/auth.service';
 
-
-
+import { addDoc, collection } from '@angular/fire/firestore';
+import { database } from './app.module';
+import { UserInfo } from './core/domin/user';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'ahmadEid';
   mobileMenu : boolean = false;
- constructor(public auth : AuthService){
+
+  public iUserInfo: UserInfo = {
+    id: 0,
+    userBrowser: '',
+    userScreenResolution: '',
+    userLanguagePreference: '',
+    userDeviceType: '',
+    userLatitudeLongitude: '',
+    userCity: '',
+    userCountry: '',
+    userTimeSpent: '',
+    userReferralSource: '',
+    userIPAddress: '',
+    userInternetServiceProvider: '',
+    userNetworkType: '',
+    userConnectionSpeed: '',
+    userDarkModePreference: '',
+    userDeviceModel: '',
+    userOperatingSystemVersion: '',
+    userBrowserVersion: '',
+    userErrorTracking: '',
+    userSessionDuration: '',
+    userNumberOfSessions: '',
+    userTime: '',
+    userLatitude: '',
+    userLongitude: ''
+  }
+ constructor(public auth : AuthService,private http: HttpClient){
 
  }
+  ngOnInit(): void {
+    this.getvisterInfo();
+    
+
+ 
+  }
+  collectionRef = collection(database, 'userInfo');
+ getvisterInfo(){
+  this.iUserInfo.userBrowser = window.navigator.userAgent;
+  this.iUserInfo.userScreenResolution = `${window.screen.width}x${window.screen.height}`;
+  this.iUserInfo.userLanguagePreference = navigator.language;
+  this.iUserInfo.userTime = new Date().toISOString();
+
+  const request = this.http.get('https://api.ipify.org/?format=json');
+  request.subscribe((res: any) => {
+    this.iUserInfo.userIPAddress = res.ip;
+    this.addUserInfoDocument();
+  });
+
+  // navigator.geolocation.getCurrentPosition(
+  //   (position) => {
+  //     this.iUserInfo.userLatitude = position.coords.latitude.toString();
+  //     this.iUserInfo.userLongitude = position.coords.longitude.toString();
+
+  //     this.addUserInfoDocument();
+  //   },
+  //   (error) => {
+  //     console.error('Error getting geolocation:', error);
+
+  //     this.addUserInfoDocument();
+  //   }
+  // );
+}
+
+addUserInfoDocument() {
+  addDoc(this.collectionRef, {
+    id: this.iUserInfo.id,
+    userBrowser: this.iUserInfo.userBrowser,
+    userScreenResolution: this.iUserInfo.userScreenResolution,
+    userLanguagePreference: this.iUserInfo.userLanguagePreference,
+    userDeviceType: this.iUserInfo.userDeviceType,
+    userLatitudeLongitude: this.iUserInfo.userLatitudeLongitude,
+    userCity: this.iUserInfo.userCity,
+    userLatitude: this.iUserInfo.userLatitude,
+    userLongitude: this.iUserInfo.userLongitude,
+    userCountry: this.iUserInfo.userCountry,
+    userTimeSpent: this.iUserInfo.userTimeSpent,
+    userReferralSource: this.iUserInfo.userReferralSource,
+    userIPAddress: this.iUserInfo.userIPAddress,
+    userInternetServiceProvider: this.iUserInfo.userInternetServiceProvider,
+    userNetworkType: this.iUserInfo.userNetworkType,
+    userConnectionSpeed: this.iUserInfo.userConnectionSpeed,
+    userDarkModePreference: this.iUserInfo.userDarkModePreference,
+    userDeviceModel: this.iUserInfo.userDeviceModel,
+    userOperatingSystemVersion: this.iUserInfo.userOperatingSystemVersion,
+    userBrowserVersion: this.iUserInfo.userBrowserVersion,
+    userErrorTracking: this.iUserInfo.userErrorTracking,
+    userSessionDuration: this.iUserInfo.userSessionDuration,
+    userNumberOfSessions: this.iUserInfo.userNumberOfSessions,
+    userTime: this.iUserInfo.userTime
+  })
+    .catch((error) => {
+      console.error('Error adding user info document:', error);
+    });
+}
+
+
+
+
   //-----------------OpenMobileMenu
     openMobileMenu () {
+  
       if (this.mobileMenu == false) {
         document.getElementById("headerMobile")!.removeAttribute("class");
         document.getElementById("headerMobile")?.classList.add("openMenuMobile");
---
+++ ahmad-eid/ahmadEid/src/app/core/domin/user.ts
@@ -0,0 +1,26 @@
+export interface UserInfo {
+    id: number ;
+    userBrowser: string ;
+    userScreenResolution: string ;
+    userLanguagePreference: string;
+    userDeviceType: string ;
+    userLatitudeLongitude : string ;
+    userCity : string ;
+    userLatitude : string ;
+    userLongitude : string ;
+    userCountry : string ;
+    userTimeSpent : string ;
+    userReferralSource   : string ;
+    userIPAddress : string ;
+    userInternetServiceProvider : string ;
+    userNetworkType : string ;
+    userConnectionSpeed : string ;
+    userDarkModePreference : string ;
+    userDeviceModel : string ;
+    userOperatingSystemVersion : string ;
+    userBrowserVersion : string ;
+    userErrorTracking : string ;
+    userSessionDuration : string ;
+    userNumberOfSessions : string ;
+    userTime : string ;
+  }
